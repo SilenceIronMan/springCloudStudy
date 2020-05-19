27ba8ed43550243f8f7b6b019583750234b1b7c3
Yinshiyu <yinshiyu_2008@126.com>
2020-05-20 01:31:07 +0800

    eureka 服务发现 以及 自我保护 和过期时间
    eureka 服务发现 内容 自我保护关闭 过期时间调整

--- cloud-provider-payment8002/src/main/java/com/ysy/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/com/ysy/springcloud/controller/PaymentController.java
@@ -5,9 +5,12 @@ import com.ysy.springcloud.entities.Payment;
 import com.ysy.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -21,6 +24,9 @@ public class PaymentController {
 
     @Resource
     private PaymentService paymentService;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @Value("${server.port}")
     private String serverPort;
@@ -45,6 +51,19 @@ public class PaymentController {
         } else {
             return new CommonResult<>(444, "查询失败:id" + id);
         }
+    }
+
+    @GetMapping("/discovery")
+    public Object discovery(){
+        List<String> services = discoveryClient.getServices();
+        for (String element : services) {
+            log.info(element);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getHost() + instance.getPort() + instance.getUri());
+        }
+        return this.discoveryClient;
     }
 
 }
