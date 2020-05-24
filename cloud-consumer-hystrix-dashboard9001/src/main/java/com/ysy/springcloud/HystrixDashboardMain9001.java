d18db17e83b99c4bbb22277d8b8ece15c824d595
Yinshiyu <yinshiyu_2008@126.com>
2020-05-24 17:59:12 +0800

    hystrix dashboard
    hystrix  监控

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/ysy/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,17 @@
+package com.ysy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @anthor silenceYin
+ * @date 2020/5/24 - 16:51
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
